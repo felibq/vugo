7c8264235e6b9371d5652cc85ff8ec49712f9532
Hejx <531601727@qq.com>
2016-11-17 23:05:48 +0800

    update slide bar

---
+++ src/previews/checkbox.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="">
+    <h1>Checkbox</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css">
+</style>
